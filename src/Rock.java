fec3f2e85ef31c10bcc9abf04c151bcb699cf7ac
Schoomie <jakubpatecki97@gmail.com>
2019-03-01 14:33:24 +0100

    1

---
+++ src/Rock.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kuba on 01/03/2019.
+ */
+public class Rock {
+
+    int uLeftPXos,uLeftYPos;
+
+    int xDirection = 1;
+    int yDirection=1;
+
+    int width = Main.boardWidth;
+}
